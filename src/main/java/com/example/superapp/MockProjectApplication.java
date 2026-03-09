37e628ebb569f095b6f251b52a636783206b026c
Phong <vanphongnguyen486@gmail.com>
2026-03-09 21:11:43 +0700

    Revert ".env file for GitGuardian"
    This reverts commit 278744d74b274c76342a10b9f7a44cb7ae1c6a6e.

--- src/main/java/com/example/superapp/MockProjectApplication.java
+++ src/main/java/com/example/superapp/MockProjectApplication.java
@@ -1,6 +1,5 @@
 package com.example.superapp;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -10,8 +9,6 @@ import org.springframework.scheduling.annotation.EnableAsync;
 public class MockProjectApplication {
 
     public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.load();
-        dotenv.entries().forEach(e -> System.setProperty(e.getKey(), e.getValue()));
         SpringApplication.run(MockProjectApplication.class, args);
     }
 }
