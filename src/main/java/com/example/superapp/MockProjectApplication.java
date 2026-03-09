278744d74b274c76342a10b9f7a44cb7ae1c6a6e
Phong <vanphongnguyen486@gmail.com>
2026-03-09 21:04:15 +0700

    .env file for GitGuardian

--- src/main/java/com/example/superapp/MockProjectApplication.java
+++ src/main/java/com/example/superapp/MockProjectApplication.java
@@ -1,5 +1,6 @@
 package com.example.superapp;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -9,6 +10,8 @@ import org.springframework.scheduling.annotation.EnableAsync;
 public class MockProjectApplication {
 
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load();
+        dotenv.entries().forEach(e -> System.setProperty(e.getKey(), e.getValue()));
         SpringApplication.run(MockProjectApplication.class, args);
     }
 }
