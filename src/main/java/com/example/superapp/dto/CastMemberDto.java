f13e666e7d3390da9660b804f46233b7bb475ed4
hieuden690@gmail.com <hieuden690@gmail.com>
2026-03-10 13:32:02 +0700

    Fix cast information on movie-details.html

---
+++ src/main/java/com/example/superapp/dto/CastMemberDto.java
@@ -0,0 +1,40 @@
+package com.example.superapp.dto;
+
+public class CastMemberDto {
+    private Long id;
+    private String name;
+    private String character;
+    private String profilePath;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCharacter() {
+        return character;
+    }
+
+    public void setCharacter(String character) {
+        this.character = character;
+    }
+
+    public String getProfilePath() {
+        return profilePath;
+    }
+
+    public void setProfilePath(String profilePath) {
+        this.profilePath = profilePath;
+    }
+}
