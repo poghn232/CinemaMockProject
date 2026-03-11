fccdaa2dd2d59095260277c4f04e420e75ea72a5
hieuden690@gmail.com <hieuden690@gmail.com>
2026-03-11 09:47:21 +0700

    fix a problem that episode in seasons that is not a number like specials can not be added

--- src/main/java/com/example/superapp/service/PublicMovieService.java
+++ src/main/java/com/example/superapp/service/PublicMovieService.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Comparator;
