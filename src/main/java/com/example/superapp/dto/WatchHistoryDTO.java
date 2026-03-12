440e582b444e2596e97a43abcc98a573ce7a0b3a
Andi <91647293+andi221-pts@users.noreply.github.com>
2026-03-12 15:16:59 +0700

    AHIHI123-37 Design the continue watching

---
+++ src/main/java/com/example/superapp/dto/WatchHistoryDTO.java
@@ -0,0 +1,22 @@
+package com.example.superapp.dto;
+
+import lombok.Data;
+import java.time.LocalDateTime;
+
+@Data
+public class WatchHistoryDTO {
+    private Long id;
+    private LocalDateTime watchedAt;
+    private Integer durationSec;
+    private Integer progressSec;
+    private String posterPath;
+
+    // Dành cho Phim Lẻ
+    private Long movieId;
+    private String movieTitle;
+
+    // Dành cho Phim Bộ
+    private Long episodeId;
+    private String tvSeriesName;
+    private String episodeName;
+}
