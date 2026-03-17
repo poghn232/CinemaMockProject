1bde22eef099976b7512f0f9672858dd30a4c61f
Phong <vanphongnguyen486@gmail.com>
2026-03-17 10:55:57 +0700

    AHIHI123-42 feat: modify banners

---
+++ src/main/java/com/example/superapp/controller/AdminBannerController.java
@@ -0,0 +1,47 @@
+package com.example.superapp.controller;
+
+import com.example.superapp.entity.Banner;
+import com.example.superapp.service.BannerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/admin/banner")
+public class AdminBannerController {
+
+    private final BannerService service;
+
+    //left-id: 2
+    @PostMapping("/upload/left")
+    public void replaceLeftBanner(@RequestParam("file")MultipartFile file,
+                                  @RequestParam String toURL) throws IOException {
+        Banner left = service.getBannerById(2);
+        left.setData(file.getBytes());
+        left.setToURL(toURL);
+        service.save(left);
+    }
+
+    //right-id: 3
+    @PostMapping("/upload/right")
+    public void replaceRightBanner(@RequestParam("file")MultipartFile file,
+                                  @RequestParam String toURL) throws IOException {
+        Banner right = service.getBannerById(3);
+        right.setData(file.getBytes());
+        right.setToURL(toURL);
+        service.save(right);
+    }
+
+    //bottomm-id: 1
+    @PostMapping("/upload/bottom")
+    public void replaceBottomBanner(@RequestParam("file")MultipartFile file,
+                                  @RequestParam String toURL) throws IOException {
+        Banner bottom = service.getBannerById(1);
+        bottom.setData(file.getBytes());
+        bottom.setToURL(toURL);
+        service.save(bottom);
+    }
+}
