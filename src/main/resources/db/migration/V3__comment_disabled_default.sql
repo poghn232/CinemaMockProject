88a59613fc87151a7215a7fdc23db39939d5cba9
hieuden690@gmail.com <hieuden690@gmail.com>
2026-04-13 14:38:04 +0700

    Bug Fix

--- src/main/resources/db/migration/V3__comment_disabled_default.sql
+++
@@ -1,8 +0,0 @@
--- Migration: ensure comment_disabled has a safe default and is NOT NULL
--- Sets existing NULL values to 0 and alters column to have NOT NULL DEFAULT 0
-
-UPDATE profiles SET comment_disabled = 0 WHERE comment_disabled IS NULL;
-ALTER TABLE profiles MODIFY COLUMN comment_disabled TINYINT(1) NOT NULL DEFAULT 0;
-
-UPDATE users SET comment_disabled = 0 WHERE comment_disabled IS NULL;
-ALTER TABLE users MODIFY COLUMN comment_disabled TINYINT(1) NOT NULL DEFAULT 0;
